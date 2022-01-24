090a39b405170d4c33b89498b4148c5bfaa63498
hash_developer <sidotidavide@gmail.com>
2022-01-24 09:10:20 +0100

    Update moto-panel.ino

--- moto-panel/moto-panel.ino
+++ moto-panel/moto-panel.ino
@@ -1,9 +1,35 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo servo;
+
+int principale = A0;
+int dx = A1;
+int sx = A2;
+int i = 0;
+
+void setup() {
+  Serial.begin(9600);
+  servo.write(0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* while (analogRead(principale) < 600) {
+    if (analogRead(dx) > 500) {
+      servo.attach(6);
+      servo.write(0);
+      Serial.println("DX");
+    }
+    else if (analogRead(sx) > 700) {
+      servo.attach(6);
+      servo.write(360);
+      Serial.println("SX");
+    }
+  } */
 
+  while (analogRead(principale) < 600) {
+    Serial.println("s");
+    servo.attach(6);
+    servo.write(i++);
+  }
+  servo.detach();
 }
