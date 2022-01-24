7f55e0982af4105d6e70b2a8f3f38ea938c5f7d1
hash_developer <sidotidavide@gmail.com>
2022-01-24 13:50:18 +0100

    Update panel_complete.ino

--- panel_complete/panel_complete.ino
+++ panel_complete/panel_complete.ino
@@ -1,9 +1,18 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+Servo servoX;
+Servo servoY;
+
+int ftUp = A0;
+int ftDown = A1;
+int ftRight = A2;
+int ftLeft = A3;
+
+void setup() {
+  Serial.begin(9600);
+  servoX.attach(3);
+  servoX.write(1);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
