196e0ab3463b9d50c2556026f2e2210546490ea0
hash_developer <sidotidavide@gmail.com>
2022-01-25 13:24:34 +0100

    up

--- panel_complete/panel_complete.ino
+++ panel_complete/panel_complete.ino
@@ -1,3 +1,12 @@
+/**
+* Project Name: Pannello fotovoltaico inseguitore
+* Project URI: -
+* Description: Un pannello fotovoltaico su una base che insegue il sole.
+* Version: 1.0
+* Author: Sidoti Davide
+* Author URI: https://sidotidavide.me/
+**/
+
 #include <Servo.h>
 
 Servo servoX;
